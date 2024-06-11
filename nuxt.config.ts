337411ee84ef767e52d30092a3362e1ff6ad6743
fadashanti <fadashanti09@gmail.com>
2024-06-11 15:22:04 +0100

    second commit

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  tailwindcss: {
+    cssPath: ['~/assets/css/tailwind.css', { injectPosition: "first" }],
+    configPath: 'tailwind.config',
+    exposeConfig: {
+      level: 2
+    },
+    config: {},
+    viewer: true,
+  },
+  modules: ["@nuxtjs/tailwindcss", "vuetify-nuxt-module"]
 })
