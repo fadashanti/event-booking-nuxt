5b325efca30faa47b1ce90216922908b5ef89cfa
fadashanti <fadashanti09@gmail.com>
2024-06-12 16:33:26 +0100

    Complete removal of TailwindCSS Module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,16 +2,6 @@
 import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
 export default defineNuxtConfig({
   devtools: { enabled: true },
-
-  tailwindcss: {
-    cssPath: ['~/assets/css/tailwind.css', { injectPosition: "first" }],
-    configPath: 'tailwind.config',
-    exposeConfig: {
-      level: 2
-    },
-    config: {},
-    viewer: true,
-  },
   
   build: {
     transpile: ['vuetify'],
@@ -23,8 +13,7 @@ export default defineNuxtConfig({
         // @ts-expect-error
         config.plugins.push(vuetify({ autoImport: true }))
       })
-    },
-    "@nuxtjs/tailwindcss"
+    }
   ],
 
   vite: {
