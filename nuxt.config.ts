baf5cdb59aec1084aa8e40c389ce519b677ef192
fadashanti <fadashanti09@gmail.com>
2024-06-12 16:28:44 +0100

    Removal of TailwindCSS

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
 export default defineNuxtConfig({
   devtools: { enabled: true },
+
   tailwindcss: {
     cssPath: ['~/assets/css/tailwind.css', { injectPosition: "first" }],
     configPath: 'tailwind.config',
@@ -10,5 +12,26 @@ export default defineNuxtConfig({
     config: {},
     viewer: true,
   },
-  modules: ["@nuxtjs/tailwindcss", "vuetify-nuxt-module"]
+  
+  build: {
+    transpile: ['vuetify'],
+  },
+
+  modules: [
+    (_options, nuxt) => {
+      nuxt.hooks.hook('vite:extendConfig', (config) => {
+        // @ts-expect-error
+        config.plugins.push(vuetify({ autoImport: true }))
+      })
+    },
+    "@nuxtjs/tailwindcss"
+  ],
+
+  vite: {
+    vue: {
+      template: {
+        transformAssetUrls,
+      },
+    },
+  },
 })
---
+++ pages/index.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="text-3xl">
+        Home Page
+    </div>
+    <div>
+        <v-btn prepend-icon="$vuetify" variant="flat" color="#16AFD8">
+            Button
+        </v-btn>
+    </div>
+</template>
